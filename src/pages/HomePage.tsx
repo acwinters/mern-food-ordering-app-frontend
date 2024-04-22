cb2f3a16a2a5856f00b54ea72636fde84395dc6e
Conor <acwinters1994@gmail.com>
2024-04-22 17:45:23 -0400

    first commit

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,23 @@
+import landingImage from "../assets/landing.png";
+import appDownloadImage from "../assets/appDownload.png";
+
+const HomePage = () => {
+	return (
+		<div className='flex flex-col gap-12'>
+			<div className='bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16'>
+				<h1 className='text-5xl font-bold tracking-tight text-orange-600'>Order takeaway today!</h1>
+				<span className='text-xl'>Food is just a click away!</span>
+			</div>
+			<div className='grid md:grid-cols-2 gap-5'>
+				<img src={landingImage} />
+				<div className='flex flex-col items-center justify-center gap-4 text-center'>
+					<span className='font-bold text-3xl tracking-tighter'>Takeaway at your fingertips!</span>
+					<span>Download the app for faster ordering today.</span>
+					<img src={appDownloadImage} />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default HomePage;
